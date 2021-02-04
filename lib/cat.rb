9337dabc3c9990cf744e0cbb7c9dfbe008d89501
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-04 17:16:24 -0500

    commit tree 18228f7e48b009cdf935ab1194a8f978fa60fb5a

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
   # code goes here
+
+  @@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner)
+      @name = name
+      @owner = owner
+      @mood = "nervous"
+      @@all << self
+      # binding.pry
+      # 0
+  end 
 end
